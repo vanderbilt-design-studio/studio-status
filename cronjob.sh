b2b8ab30f32a31bd175cf1dcc405c2a7fc9903a5
Sameer Puri <spuri4096@gmail.com>
2017-09-17 00:05:20 -0500

    Add cronjob

---
+++ cronjob.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "studio_status up to date"
+elif [ $LOCAL = $BASE ]; then
+    echo "studio_status updating..."
+    git pull
+    kill `pgrep -f processing`
+    processing-java --sketch=`pwd` --run &
+elif [ $REMOTE = $BASE ]; then
+    echo 'Origin is behind, huh?'
+else
+    echo 'Diverged!'
+fi
+
+
